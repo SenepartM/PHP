ca605c78bd5818126f8a3365641fa5f271b046cc
SenepartM <113522450+SenepartM@users.noreply.github.com>
2023-01-25 17:44:30 +0100

    bddddd

---
+++ BDD.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BASE DE DONNNNNNNNNNEE</title>
+    
+</head>
+<body>
+   <h1>MALADE ?  PRENEZ RENDEZ-VOUS </h1>
+    <br><br><br><br><br><br>
+<br><br>
+<?php 
+
+
+try {
+   $ipserver="192.168.64.206";
+   $nomBase="Medecin";
+   $loginPrivilege ="root";
+   $passPrivilege="root";
+   $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege); 
+
+?>
+    <?php
+   $requete= "select * from Medecin";
+   $requete1= "select * from Patient";
+   $resultat=$GLOBALS["pdo"]->query($requete);
+   $resultat1=$GLOBALS["pdo"]->query($requete1);
+ $tabMedecin = $resultat->fetchALL();
+ $tabPatient = $resultat1->fetchALL();
+ ?>
+ <form action=""method="post">
+
+ <label for="idMedecin">Medecin:</label>
+ <select name="idMedecin">
+    <?php
+foreach ($tabMedecin as $Medecin){
+    ?>
+          <option value="<?=$Medecin["id"]?>"><?=$Medecin["nom"]." ".$Medecin["prenom"]?></option>
+          <?php
+  }?>
+ </select><br>
+<label for="idPatient">Patient:</label>
+ <select name="idPatient">
+<?php foreach ($tabPatient as $Patient){
+    ?>
+          <option value="<?=$Patient["id"]?>"><?=$Patient["nom"]." ".$Patient["prenom"]?></option>
+          <?php
+  }?>
+ </select><br>
+ <label for="time">TIME:</label>
+
+<input type="time" id="time" name="time"><br>
+<input type="submit" value="Send Request" />
+</form>
+<?php
+if (isset($_POST["idMedecin"])&&($_POST["idPatient"])) {
+    echo "id du medecin :". $_POST["idMedecin"];  
+    echo "<br>";
+    echo "id du patient :". $_POST["idPatient"];  
+    $time= $_POST['time'];
+    $idMedecin =$_POST['idMedecin'];
+    $idPatient = $_POST['idPatient'];
+    $requete2="INSERT INTO Consultation (Dateheure,idMedecin,idPatient)
+    Values ('time','idMedecin','idPatient'";
+} 
+
+
+} catch (Exception  $error) {
+    echo "error est : ".$error->getMessage();
+}
+?>
+</body>
+</html>
