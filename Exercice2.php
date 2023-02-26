d45fc224b0476783b07274c234a15eb20c56dca8
SenepartM <113522450+SenepartM@users.noreply.github.com>
2023-02-26 06:11:32 +0100

    PHP OBJET EXO 2

--- Exercice2.php
+++ Exercice2.php
@@ -1,13 +1,26 @@
-<?php echo "Coucou" ;?>
-<br><br><br><br><br>
-
-<?php function NewTable($x,$y,$z){
-    echo "<table border=2><tr><th>".$x."</th><td>".$y."</td><td>".$z."</td></tr><tr><td>Race</td><td>Jack Russell</td><td>Poodle</td></tr></table>";
+<?php
+// pour show les erreurs php sans aller changer la config, jl'ai jamais fait chez moi.. 
+error_reporting(E_ALL);    
+ini_set("display_errors", 1);
+class Personnage {
+    private $Speudo;
+    private $Vie;
+    
+    public function __construct() {
+        $this->Vie = 100;
+    }
 }
+//INSTANCE
+$user1 = new Personnage(); 
 
-$a = "1st";
-$b = "2nd";
-$c = "3rd";
-NewTable($a,$b,$c);?>
- <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
- <?php NewTable($a,$b,$c); ?>
+
+
+/*  pour faire tout ca il faudrait des propriétés public comme on passe pas par des méthodes de changement de nom .. mais pas safe
+?><?php
+// On lui donne un blaz
+$user1->Speudo = "KEKW";
+
+// Afficher le pseudo et la vie du personnage
+echo "Pseudo : " . $user1->Speudo . "<br>"; // pas bien de br c est pas google friendly mais c est pratique quand meme !!!
+echo "Vie : " . $user1->Vie; 
+?>*/
